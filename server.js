81bb8fff3f08030920e8aeb1025f509ef78753f1
prompter <prompter@local>
2026-06-30 13:15:38 +0900

    화면 꺼짐 방지(Wake Lock) 추가: 프롬프터 화면 항상 켜둠

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ const MIME = {
 };
 
 // 배포된 버전 확인용. 코드 바꿀 때마다 올린다.
-const BUILD = "2026-06-18-vflip-order";
+const BUILD = "2026-06-18-wakelock";
 
 function lanIP() {
   const ifaces = os.networkInterfaces();
